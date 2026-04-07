c08510f2a6680ed4483a954b943e4eb032cd6e3a
logan-gameing <Logan.Jones1@edu.sait.ca>
2026-04-07 13:10:05 -0600

    Added search bar to other pages where it is needed

--- app/public/events/page.tsx
+++ app/public/events/page.tsx
@@ -1,4 +1,4 @@
-import EventCard from "@/components/EventsCard";
+import SearchBar from "@/components/SearchEvent";
 import { adminDb } from "@/lib/firebaseAdmin";
 
 export default async function EventsPage() {
@@ -9,11 +9,7 @@ export default async function EventsPage() {
     <>
       <main className="max-w-6xl mx-auto p-6">
         <h1 className="text-2xl font-bold mb-4">All Events</h1>
-        <div className="grid grid-cols-3 gap-4">
-          {events.map((e) => (
-            <EventCard key={e.id} event={e} />
-          ))}
-        </div>
+        <SearchBar events={events}/>
       </main>
     </>
   );
